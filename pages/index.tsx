5488bce33fbea589cad020c2283250a96c7dc165
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2024-01-16 02:04:48 +0530

    Added Header Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,24 @@
 import Link from "next/link";
+import type {NextPage} from 'next'
 import Head from "next/head"
-import Layout from "../components/Layout";
+import Header from "../components/Header"
 
-const IndexPage = () => {
+const Home: NextPage = () => {
   return(
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Home</title>
       </Head>
-      Lets Build Netflix
+      <Header/>
       <main>
         {/* Banner */}
         <section>
           {/* row */}
         </section>
       </main>
+      {/* modal */}
     </div>
   )
   };
 
-export default IndexPage;
+export default Home;
